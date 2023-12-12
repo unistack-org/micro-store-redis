b263b690313d5b9b1d3d458ec9abb91f153ad5c7
Vasiliy Tolstov <v.tolstov@unistack.org>
2023-12-12 13:48:47 +0300

    add ability to get *redis.Client
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- redis.go
+++ redis.go
@@ -43,6 +43,10 @@ func (r *Store) Init(opts ...options.Option) error {
 	return r.configure()
 }
 
+func (r *Store) Redis() *redis.Client {
+	return r.cli.(*redis.Client)
+}
+
 func (r *Store) Disconnect(ctx context.Context) error {
 	return r.cli.Close()
 }
